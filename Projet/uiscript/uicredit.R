ea9dcc3fc6cac48e5e1af53c5da6b0969a4f1747
AimeneHjj <ahejjaj@etu.isara.fr>
2026-02-10 14:33:15 +0100

    Rajout_Crédits

--- Projet/uiscript/uicredit.R
+++ Projet/uiscript/uicredit.R
@@ -2,6 +2,13 @@ fluidRow(
   column(10, offset = 1,
          br(),
          
+         # Description générale
+         p("Merci d'avoir utilisé notre application. Shiny est un framework R
+         développé par RStudio qui permet de créer des applications web
+           interactives directement depuis R.", 
+           style = "text-align: justify; font-size: 16px;"),
+         br(),
+         
          # Section Équipe
          wellPanel(
            h4("L'Équipe de Création", style = "margin-top: 0; color: #2c3e50;"),
@@ -12,7 +19,6 @@ fluidRow(
              tags$li(strong("Amir VELLUT")),
              tags$li(strong("Brayane SIMO"))
            )
-         ),
-         
+         )
   )
 )
