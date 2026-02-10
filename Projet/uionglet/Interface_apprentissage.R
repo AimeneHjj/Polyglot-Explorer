690f671bf3b444672f099e7a86cf1a380f884136
louis-rmr <lromero@etu.isara.fr>
2026-02-10 12:29:51 +0100

    ajout script ui onglet apprentissage

---
+++ Projet/uionglet/Interface_apprentissage.R
@@ -0,0 +1,56 @@
+
+library(shiny)
+
+fluidPage(
+  
+  titlePanel("Apprentissage"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      selectInput(
+        "Langue",
+        "Langue",
+        choices = c(
+          "Anglais", "Espagnol", "Allemand",
+          "Italien", "Arabe", "Chinois", "Portugais"
+        )
+      ),
+      
+      checkboxGroupInput(
+        "Sens",
+        "Sens de traduction",
+        choices = c(
+          "Français → Anglais",
+          "Anglais → Français"
+        )
+      ),
+      
+      checkboxGroupInput(
+        "Mode",
+        "Mode d'apprentissage",
+        choices = c("random", "Renfo")
+      ),
+      
+      selectInput(
+        "categorie",
+        "Catégorie",
+        choices = c(
+          "Adverbe", "Animaux", "Meuble",
+          "Pronom", "Temps", "Verbe", "Verbe irrégulier"
+        )
+      )
+    ),
+    
+    mainPanel(
+      textOutput("Traduction"),
+      textInput(
+        "Exo",
+        "Traduire le mot : "),
+      submitButton("Valider"),
+      textOutput("Réponse")
+      
+      )
+    )
+  )
