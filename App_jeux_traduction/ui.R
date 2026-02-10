eb7d4d584219c989af36df1e5e5846b4253e7da4
louis-rmr <lromero@etu.isara.fr>
2026-02-10 10:11:33 +0100

    ajout du tabpanel pour intergrer dans tabsetpanel

--- App_jeux_traduction/ui.R
+++ App_jeux_traduction/ui.R
@@ -1,13 +1,15 @@
 library(shiny)
 
-fluidPage(
-  h3("Ajouter un mot"), #affiche le titre
-  textInput("mot_fr", "Mot"), #zone d'écriture du mot en français
-  textInput("mot_et", "Mot a traduire"),# zone d'écriture du mot traduit
-  textInput("nouvelle_categorie", "Nouvelle catégorie"), # zone d'écriture de la catégorie
-  selectInput("categorie_existante", "Catégorie",
-              choices = c("Animaux", "Verbes", "Objets")),# liste déroulante catégorie
-  textInput("nouvelle_langue", "Nouvelle langue"),#zone d'écriture de la langue 
-  selectInput("langue", "Langue",
-              choices = c("Anglais")),# liste déroulante de la langue 
-  actionButton("Valider_mot", "Valider") # bonton pour valider
+tabPanel("Paramettrage",
+         fluidPage(
+           h3("Ajouter un mot"), #affiche le titre
+           textInput("mot_fr", "Mot"), #zone d'écriture du mot en français
+           textInput("mot_et", "Mot a traduire"),# zone d'écriture du mot traduit
+           textInput("nouvelle_categorie", "Nouvelle catégorie"), # zone d'écriture de la catégorie
+           selectInput("categorie_existante", "Catégorie",
+                       choices = c("Animaux", "Verbes", "Objets")),# liste déroulante catégorie
+           textInput("nouvelle_langue", "Nouvelle langue"),#zone d'écriture de la langue 
+           selectInput("langue", "Langue",
+                       choices = c("Anglais")),# liste déroulante de la langue 
+           actionButton("Valider_mot", "Valider") # bonton pour valider
+         ))
