58923d2ab682b6fd5f6a1db8f089ea4d6c50ed4d
AimeneHjj <ahejjaj@etu.isara.fr>
2026-02-10 12:08:29 +0100

    Onglet Apprentissage

--- ApplicationAïmène/ui.R
+++ ApplicationAïmène/ui.R
@@ -7,30 +7,34 @@ fluidPage(
   titlePanel("Application de Traduction"),
   
   # Création des 4 onglets
-  sidebarLayout(
-    tabsetPanel(
-      tabPanel("Présentation",
-               "Contenu de l'onglet Présentation")
-      tabPanel("Apprentissage",
-               "Contenu de l'onglet Apprentissage")
-      tabPanel("Paramettrage",
-               fluidPage(
-                 sidebarLayout(
-                   sidebarPanel(
-                     selectInput("categorie_existante", "Catégorie",
-                                 choices = c("Animaux", "Verbes", "Objets")),# liste déroulante catégorie
-                     textInput("nouvelle_categorie", "Nouvelle catégorie"), # zone d'écriture de la catégorie
-                     selectInput("langue", "Langue",
-                                 choices = c("Anglais")),# liste déroulante de la langue 
-                     textInput("nouvelle_langue", "Nouvelle langue"),#zone d'écriture de la langue
-                   ),
-                   mainPanel(
-                     h3("Ajouter un mot"), #affiche le titre
-                     textInput("mot_fr", "Mot"), #zone d'écriture du mot en français
-                     textInput("mot_et", "Mot a traduire"),# zone d'écriture du mot traduit
-                     actionButton("Valider_mot", "Valider") # bonton pour valider
-                   )
-                 )))
-      tabPanel("Crédits","Contenu de l'onglet Crédits")
-    )
+  tabsetPanel(
+    tabPanel("Présentation",
+             "Contenu de l'onglet Présentation"),
+    
+    tabPanel("Apprentissage",
+             "Contenu de l'onglet Apprentissage"),
+    
+    tabPanel("Paramétrage",
+             fluidPage(
+               sidebarLayout(
+                 sidebarPanel(
+                   selectInput("categorie_existante", "Catégorie",
+                               choices = c("Animaux", "Verbes", "Objets")), # liste déroulante catégorie
+                   textInput("nouvelle_categorie", "Nouvelle catégorie"), # zone d'écriture de la catégorie
+                   selectInput("langue", "Langue",
+                               choices = c("Anglais")), # liste déroulante de la langue 
+                   textInput("nouvelle_langue", "Nouvelle langue") #zone d'écriture de la langue
+                 ),
+                 mainPanel(
+                   h3("Ajouter un mot"), #affiche le titre
+                   textInput("mot_fr", "Mot"), #zone d'écriture du mot en français
+                   textInput("mot_et", "Mot à traduire"), # zone d'écriture du mot traduit
+                   actionButton("Valider_mot", "Valider") # bouton pour valider
+                 )
+               )
+             )),
+    
+    tabPanel("Crédits",
+             "Contenu de l'onglet Crédits")
   )
+)
